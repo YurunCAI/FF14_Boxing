16a5e225c88b0da73c5c4dad1be7a2bf56972445
YurunCAI <c911470938@gmail.com>
2021-12-06 11:04:57 +1300

    FF14 Boxing

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace FF14Felling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblCheck = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // colorBtn
+            // 
+            this.colorBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colorBtn.Location = new System.Drawing.Point(12, 23);
+            this.colorBtn.Name = "colorBtn";
+            this.colorBtn.Size = new System.Drawing.Size(105, 33);
+            this.colorBtn.TabIndex = 0;
+            this.colorBtn.Text = "Start";
+            this.colorBtn.UseVisualStyleBackColor = true;
+            this.colorBtn.Click += new System.EventHandler(this.colorBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(133, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 18);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.Location = new System.Drawing.Point(26, 84);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(48, 18);
+            this.lblCount.TabIndex = 5;
+            this.lblCount.Text = "Count";
+            // 
+            // lblCheck
+            // 
+            this.lblCheck.AutoSize = true;
+            this.lblCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCheck.Location = new System.Drawing.Point(130, 84);
+            this.lblCheck.Name = "lblCheck";
+            this.lblCheck.Size = new System.Drawing.Size(50, 18);
+            this.lblCheck.TabIndex = 6;
+            this.lblCheck.Text = "Status";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(145, 23);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(105, 33);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(133, 139);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(130, 18);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(133, 163);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(130, 18);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Left Check";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 18);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Right Check";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 18);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "ESC Check";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 367);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblCheck);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.colorBtn);
+            this.Name = "Form1";
+            this.Text = "FF14 Felling Dev";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button colorBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label lblCheck;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace FF14Felling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        int n = 150;
+        int r, g, b, r2, g2, b2, r3, g3, b3;
+        bool colorCheck = false;
+        bool controlFlag = false;
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDC(IntPtr hwnd);
+        [DllImport("user32.dll")]
+        private static extern Int32 ReleaseDC(IntPtr hwnd, IntPtr hdc);
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(Byte bVk, Byte bScan, Int32 dwFlags, Int32 dwExtraInfo);
+        [DllImport("gdi32.dll")]
+        private static extern int GetPixel(IntPtr hdc, Point p);
+
+
+
+        //click botton to start
+        private void colorBtn_Click(object sender, EventArgs e)
+        {
+            controlFlag = true;
+            maintain(sender, e);
+        }
+
+
+        Timer countDown_0 = new Timer();
+        Timer countDownFelling = new Timer();
+        //maintain
+        private void maintain(object sender, EventArgs e)
+        {
+            //press 0
+            
+            countDown_0.Tick += PressZero;
+            countDown_0.Enabled = true;
+            countDown_0.Interval = 10;
+            countDown_0.Start();
+
+
+        }
+
+        private void PressZero(object sender, EventArgs e)
+        {
+            count++;
+            lblCount.Text = count.ToString();
+            if (count == n) { keybd_event(96, 1, 0, 0); keybd_event(96, 1, 2, 0); lblCheck.Text = "Press 0"; } //select boxing machine
+            else if (count == n + 25) { keybd_event(96, 1, 0, 0); keybd_event(96, 1, 2, 0); lblCheck.Text = "Press 0"; } //start boxing machine
+            else if (count == n + 100) { keybd_event(98, 1, 0, 0); keybd_event(98, 1, 2, 0); lblCheck.Text = "Press 2"; } //go confirm
+            else if (count == n + 125) { keybd_event(96, 1, 0, 0); keybd_event(96, 1, 2, 0); lblCheck.Text = "Press 0"; }//confirm
+            else if (count > n + 175 && count < n + 300)
+            {
+                //Felling
+                Felling(sender, e);
+            }
+            else if (count > n + 400)
+            {
+
+                Point p3 = new Point(822, 512);
+                IntPtr hdc3 = GetDC(IntPtr.Zero);
+                int c3 = GetPixel(hdc3, p3);
+                r3 = (c3 & 0xFF);
+                g3 = (c3 & 0xFF00) / 256;
+                b3 = (c3 & 0xFF0000) / 65536;
+                pictureBox3.BackColor = Color.FromArgb(r3, g3, b3);
+                if (r3 > 130 && r3 < 145 && g3 > 95 && g3 < 110 && b3 > 70 && b3 < 80)
+                {
+                    keybd_event(111, 0, 0, 0); keybd_event(111, 0, 2, 0);//esc
+                    count = 100;
+                    ReleaseDC(IntPtr.Zero, hdc3);
+
+                    countDown_0.Tick -= PressZero;
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    countDown_0.Tick += PressZero;
+                }
+                else
+                {
+                    count = 100;
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                }
+            }
+        }
+
+        private void Felling(object sender, EventArgs e)
+        {
+            Point p = new Point(907, 969); // get position
+            IntPtr hdc = GetDC(IntPtr.Zero);
+            Point p2 = new Point(940, 969); //get second position
+            IntPtr hdc2 = GetDC(IntPtr.Zero);
+            int c = GetPixel(hdc, p); //get color
+            int c2 = GetPixel(hdc2, p2);                          
+            r = (c & 0xFF);
+            g = (c & 0xFF00) / 256;
+            b = (c & 0xFF0000) / 65536;
+            r2 = (c2 & 0xFF);
+            g2 = (c2 & 0xFF00) / 256;
+            b2 = (c2 & 0xFF0000) / 65536;
+            pictureBox1.BackColor = Color.FromArgb(r, g, b);
+            pictureBox2.BackColor = Color.FromArgb(r2, g2, b2);
+            ColorCheck(r, g, b, r2, g2, b2);
+            if (colorCheck) //left the qq window rgb
+            {
+                lblCheck.Text = "Check";
+                keybd_event(96, 0, 0, 0);
+                keybd_event(96, 0, 2, 0);
+                ReleaseDC(IntPtr.Zero, hdc);
+                ReleaseDC(IntPtr.Zero, hdc2);
+            }
+            else
+            {
+                lblCheck.Text = "UnCheck";
+            }
+        }
+
+        private void ColorCheck(int r, int g, int b, int r2, int g2, int b2)
+        {
+            //r
+            if (r > 202 && r < 212 && r2 >87 && r2 <97)
+            {
+                //g
+                if (g > 121 && g < 131 )
+                {
+                    //b
+                    if (b > 33 && b < 42) colorCheck = true;
+                }
+            }
+            else
+            {
+                colorCheck = false;
+            }
+        }
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (controlFlag)
+            {
+                count = 0;
+                countDown_0.Dispose();
+            }
+        }
+    }
+}
